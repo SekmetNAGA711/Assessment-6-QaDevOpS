cfc72feae247643bc1d604e3277e34e0782fce7c
AbbyANG <107139513+SekmetNAGA711@users.noreply.github.com>
2023-08-21 11:22:25 -0400

    Update shuffle.test.js

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,11 +1,25 @@
 const shuffle = require("../src/shuffle");
 
-describe("shuffle should...", () => {
-  // CODE HERE
-  test("Shuffle should return an array", ()=>{
-   expect(shuffle()).tobe()
-  })
-  test("Shuffle should return same length of an array", ()=>{
-    expect(shuffle([1, 2, 3])).toEqual([1, 2, 3])
-  })
+describe("Shuffle should...", ()=>{
+  test('shuffle shuffles the array', () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    const shuffledArray = shuffle(inputArray.slice());
+  
+    // Check if the length remains the same
+    expect(shuffledArray.length).toBe(inputArray.length);
+  
+    // Check if the content is different from the input array
+    expect(shuffledArray).not.toEqual(inputArray);
+  
+    // Check if the shuffled array contains the same elements as the input array
+    expect(shuffledArray.sort()).toEqual(inputArray.sort());
+  });
+  
+  test('shuffleArray handles empty arrays', () => {
+    const emptyArray = [];
+    const shuffledArray = shuffle(emptyArray.slice());
+  
+    expect(shuffledArray.length).toBe(0);
+    expect(shuffledArray).toEqual([]);
+  });
 });
